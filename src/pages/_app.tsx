7490f1da1bf36181fb44ef778d3c0d39ae3afcd1
Vinicius Gomes <vinicius.svgomes@gmail.com>
2021-06-21 01:35:30 -0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app'
+import '../services/firebase'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
